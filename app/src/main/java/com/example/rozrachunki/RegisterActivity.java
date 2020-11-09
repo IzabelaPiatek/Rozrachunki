9df033f3604e01f3dc47877dbdedd0be15a215d9
Iza <izabela.k.piatek@gmail.com>
2020-11-09 18:23:36 +0100

    Dodanie interfejsu logowania i rejestracji

---
+++ app/src/main/java/com/example/rozrachunki/RegisterActivity.java
@@ -0,0 +1,32 @@
+package com.example.rozrachunki;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getSupportActionBar().hide();
+
+        TextView zaloguj = findViewById(R.id.zalogujsieAR);
+        zaloguj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), LoginActivity.class);
+                view.getContext().startActivity(intent);
+            }
+        });
+    }
+
+
+
+}
+
+
