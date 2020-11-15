a816f9acfa982d290d17c1c6157f43fa5e644368
Emilia <emila1885@gmail.com>
2020-11-15 22:47:40 +0100

    Roboczo działa dodawanie użytkowników do bazy

--- app/src/main/java/com/example/rozrachunki/LoginActivity.java
+++ app/src/main/java/com/example/rozrachunki/LoginActivity.java
@@ -4,16 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.rozrachunki.model.User;
+import com.example.rozrachunki.remote.ApiUtils;
+import com.example.rozrachunki.services.UserService;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -23,10 +24,40 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
 
+        UserService userService = ApiUtils.getUserService();
+
         Button logowanie = findViewById(R.id.loginbtn);
         logowanie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                Call<User> call = userService.message();
+                call.enqueue(new Callback<User>() {
+                    @Override
+                    public void onResponse(Call<User> call, Response<User> response) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<User> call, Throwable t) {
+                        Toast.makeText(LoginActivity.this,t.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                });
+
+
+
+                Call<User> call2 = userService.register(new User(null, "alala", "aakka@wp.pl", "haselko", "236785453"));
+                call2.enqueue(new Callback<User>() {
+                    @Override
+                    public void onResponse(Call<User> call2, Response<User> response) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<User> call2, Throwable t) {
+                        Toast.makeText(LoginActivity.this,t.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                });
                 //Intent intent = new Intent(view.getContext(), RegisterActivity.class);
                 //view.getContext().startActivity(intent);
             }
---
+++ app/src/main/java/com/example/rozrachunki/services/UserService.java
@@ -0,0 +1,29 @@
+package com.example.rozrachunki.services;
+
+import com.example.rozrachunki.model.User;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.HTTP;
+import retrofit2.http.Path;
+
+public interface UserService {
+
+    @GET("login/{username}/{password}")
+    Call<User> login(@Path("username") String username,
+                     @Path("password") String password);
+
+    //@GET("users/save")
+    @HTTP(method = "POST", path = "users/save", hasBody = true)
+    Call<User> register(@Body User user);
+
+    @GET("mobile/hello")
+    Call<User> message();
+
+//    @HTTP(method = "POST", path = "send/{instructor}", hasBody = true)
+//    Call<Message> sendCoordinates(@Body ArrayList<LocationToSend> coordinates,
+//                                  @Path("instructor") Integer currentLoggedInstructorId);
+//    @GET("timetable/{id}")
+//    Call<ArrayList<TimetableJson>> getTodayTimetable(@Path("id") Integer instructorId);
+}
